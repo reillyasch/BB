1df633ed49eea7c65f8016e888596a93d5310d32
ReillyAsch <93880626+ReillyAsch@users.noreply.github.com>
2022-06-07 21:21:20 -0500

    commit

--- js/form.js
+++ js/form.js
@@ -1,56 +1,18 @@
 window.addEventListener("load", function() {
-    const form = document.getElementById('companyInterestForm');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      /*Comment out later*/
-      .then(() => {
-        /*alert("Success!");*/
-        form.reset();
-        return false;
-      })
+  const form = document.getElementById('contactForm');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      const inputs = document.querySelectorAll('#name, #email, #comments')
+      inputs.forEach(input => {
+        input.value='';
+      });
     });
   });
-
-  window.addEventListener("load", function() {
-    const form = document.getElementById('studentInterestForm');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      /*Comment out later*/
-      .then(() => {
-        /*alert("Success!");*/
-        form.reset();
-        return false;
-      })
-    });
-  });
-
-  window.addEventListener("load", function() {
-    const form = document.getElementById('newsletter');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      /*Comment out later*/
-      .then(() => {
-        /*alert("Success!");*/
-        form.reset();
-        return false;
-      })
-    });
-  });
+});
