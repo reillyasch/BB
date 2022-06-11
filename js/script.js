4275b959acb2b374496901245e87dd168f86f37b
ReillyAsch <93880626+ReillyAsch@users.noreply.github.com>
2022-06-11 11:52:03 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu  = document.querySelector(".nav-menu");
+const nav_ul  = document.querySelector("ul");
 const header = document.querySelector("header");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu(){
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    nav_ul.classList.toggle("active");
     header.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav-link");
+const nav_a = document.querySelectorAll("nav ul li a");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+nav_a.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu(){
-    navMenu.classList.remove("active");
     hamburger.classList.remove("active");
+    nav_ul.classList.remove("active");
     header.classList.toggle("active");
 }
